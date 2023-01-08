f1260344fd1333931e44bd514e14f13fa41cdbd5
Muneshwar-ML <muneshwarkatokar@gmail.com>
2023-01-08 19:41:36 +0530

    Test

---
+++ Test/src/com/test/Test1.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("test");
+		System.out.println("test 22");
+		System.out.println("test 33");
+
+	}
+
+}
